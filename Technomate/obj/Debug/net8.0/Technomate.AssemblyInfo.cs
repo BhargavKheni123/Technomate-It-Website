2dfe5dda0b9ecdf3db7b484f62c9e3e46c391919
vaidehipatel066 <vaidehipatel066@gmail.com>
2026-01-16 11:15:32 +0530

    changes

--- Technomate/obj/Debug/net8.0/Technomate.AssemblyInfo.cs
+++ Technomate/obj/Debug/net8.0/Technomate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Technomate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744bd307cf6b39693c35508b24e8b6330b20da49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc9c1ca5a41526f8fada18e10437f9009355563")]
 [assembly: System.Reflection.AssemblyProductAttribute("Technomate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Technomate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
